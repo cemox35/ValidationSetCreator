0e7aae7a074a5148667a6be4daeff9dcb0b4bba7
Cem Ayar <ayarcem@gmail.com>
2022-02-12 22:07:22 +0300

    Added

---
+++ dataset_analyser.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+db0_excel = pd.read_excel("valset_log.xlsx", sheet_name="dB0")
+db5_excel = pd.read_excel("valset_log.xlsx", sheet_name="dB5")
+db10_excel = pd.read_excel("valset_log.xlsx", sheet_name="dB10")
+db15_excel = pd.read_excel("valset_log.xlsx", sheet_name="dB15")
+
+print(db0_excel[1].value_counts())
+print(db5_excel[1].value_counts())
+print(db10_excel[1].value_counts())
+print(db15_excel[1].value_counts())
