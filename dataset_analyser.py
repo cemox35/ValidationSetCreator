8c4e02d88b67bf9c3fc6393536cb15ed65b4387f
Cem Ayar <ayarcem@gmail.com>
2022-02-27 01:26:56 +0300

    modified vbd splitter

--- dataset_analyser.py
+++ dataset_analyser.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-db0_excel = pd.read_excel("valset_log.xlsx", sheet_name="dB0")
-db5_excel = pd.read_excel("valset_log.xlsx", sheet_name="dB5")
-db10_excel = pd.read_excel("valset_log.xlsx", sheet_name="dB10")
-db15_excel = pd.read_excel("valset_log.xlsx", sheet_name="dB15")
+PATH = "Dataset/"
+
+db0_excel = pd.read_excel(PATH + "valset_log.xlsx", sheet_name="dB0")
+db5_excel = pd.read_excel(PATH + "valset_log.xlsx", sheet_name="dB5")
+db10_excel = pd.read_excel(PATH + "valset_log.xlsx", sheet_name="dB10")
+db15_excel = pd.read_excel(PATH + "valset_log.xlsx", sheet_name="dB15")
 
 print(db0_excel[1].value_counts())
 print(db5_excel[1].value_counts())
--- val_set_creator.py
+++ val_set_creator.py
@@ -9,17 +9,43 @@ OLD_CLEAN_PATH = PATH + "clean_trainset_wav/"
 NEW_NOISY_PATH = PATH + "noisy_valset_wav/"
 NEW_CLEAN_PATH = PATH + "clean_valset_wav/"
 
+classes = [
+    "kitchen",
+    "cafeteria",
+    "traffic",
+    "meeting",
+    "ssn",
+    "station",
+    "car",
+    "metro",
+    "restaurant",
+    "babble"
+]
 
-def get_sample_from_txt(samples):
-    cont = (samples[2] == 0)
-    temp_db0 = samples[cont].sample(n=75)
-    cont = (samples[2] == 5)
-    temp_db5 = samples[cont].sample(n=75)
-    cont = (samples[2] == 10)
-    temp_db10 = samples[cont].sample(n=75)
-    cont = (samples[2] == 15)
-    temp_db15 = samples[cont].sample(n=75)
+db_levels = [0, 5, 10, 15]
 
+
+def create_samples(samples, db, cl, quantity):
+    cont = (samples[2] == db)
+    samples = samples[cont]
+    cont = (samples[1] == cl)
+    samples = samples[cont].sample(n=quantity)
+    return samples
+
+
+def get_db_samples_from_txt(temp_db0, temp_db5, temp_db10, temp_db15, samples):
+    for di in range(0, len(db_levels)):
+        for ci in range(0, len(classes)):
+            temp_sample = create_samples(samples, db_levels[di], classes[ci], 8)
+
+            if di == 0:
+                temp_db0 = pd.concat([temp_db0, temp_sample])
+            elif di == 1:
+                temp_db5 = pd.concat([temp_db5, temp_sample])
+            elif di == 2:
+                temp_db10 = pd.concat([temp_db10, temp_sample])
+            else:
+                temp_db15 = pd.concat([temp_db15, temp_sample])
     return temp_db0, temp_db5, temp_db10, temp_db15
 
 
@@ -32,7 +58,6 @@ def write_excel(temp_db0, temp_db5, temp_db10, temp_db15):
 
 
 def copy_files(temp_db0, temp_db5, temp_db10, temp_db15):
-
     if not os.path.exists(NEW_NOISY_PATH):
         os.makedirs(NEW_NOISY_PATH)
 
@@ -52,11 +77,19 @@ def copy_files(temp_db0, temp_db5, temp_db10, temp_db15):
         shutil.move(OLD_NOISY_PATH + x + ".wav", NEW_NOISY_PATH + x + ".wav")
         shutil.move(OLD_CLEAN_PATH + x + ".wav", NEW_CLEAN_PATH + x + ".wav")
 
+
 if __name__ == '__main__':
     log_file = "/log_trainset_28spk.txt"
 
+    dB0 = pd.DataFrame()
+    dB5 = pd.DataFrame()
+    dB10 = pd.DataFrame()
+    dB15 = pd.DataFrame()
+
     data = pd.read_csv(PATH + log_file, sep=" ", header=None)
 
-    dB0, dB5, dB10, dB15 = get_sample_from_txt(data)
-    write_excel(dB0, dB5, dB10, dB15)
+    dB0, dB5, dB10, dB15 = get_db_samples_from_txt(dB0, dB5, dB10, dB15, data)
+
     copy_files(dB0, dB5, dB10, dB15)
+    write_excel(dB0, dB5, dB10, dB15)
+
